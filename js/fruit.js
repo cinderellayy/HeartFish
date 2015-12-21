5fa18e2484f6000027ae77bd930124f848ede894
yuying <yuying@itjuzi.com>
2015-12-21 10:57:36 +0800

    果实上浮上.

--- js/fruit.js
+++ js/fruit.js
@@ -2,9 +2,13 @@
 var fruitObj = function() {
     this.x = [];
     this.y = [];
-     this.alive = [];
+    this.alive = [];
+    //果实上浮
+    this.l = [];
     this.orange = new Image();
     this.blue = new Image();
+
+
 }
 fruitObj.prototype.num = 30;
 fruitObj.prototype.init = function() {
@@ -12,24 +16,27 @@ fruitObj.prototype.init = function() {
         this.alive[i] = true;
         this.x[i] = 0;
         this.y[i] = 0;
+        this.l[i] = 0;
         this.born(i);
+
     }
     this.orange.src = "./src/fruit.png";
     this.blue.src = "./src/blue.png"
 }
 fruitObj.prototype.draw = function() {
-    
+
     for (var i = 0; i < this.num; i++) {
-        ctx2.drawImage(this.orange, this.x[i] - this.orange.width*0.5 , this.y[i] - this.orange.height*0.5 );
-        
+        this.l[i] += 0.01 * deltaTime;
+        ctx2.drawImage(this.orange, this.x[i] - this.l[i] * 0.5, this.y[i] - this.l[i] * 0.5, this.l[i], this.l[i]);
+
     }
 
 }
 fruitObj.prototype.born = function(i) {
-    // body...
-    var aneID = Math.floor(Math.random() * ane.num);
+// body...
+var aneID = Math.floor(Math.random() * ane.num);
+this.x[i] = ane.x[aneID];
+this.y[i] = ane.len[aneID] - canHeight;
+this.l[i] = 0;
 
-    this.x[i] = ane.x[aneID];
-    this.y[i] = ane.len[aneID] - canHeight;
-   
 }
