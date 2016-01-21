ba4e270fe835d8361d8daab5ba84cde9362bc99f
Agzdjy <agzdjyxyy@outlook.com>
2016-01-21 19:12:06 +0800

    format code

--- js/main.js
+++ js/main.js
@@ -33,109 +33,106 @@ var halo;
 document.body.onload = game;
 
 function game() {
-    init();
-    lastTime = Date.now();
-    deltaTime = 0;
-    gameloop();
+  init();
+  lastTime = Date.now();
+  deltaTime = 0;
+  gameloop();
 }
 
 function init() {
-    can1 = document.getElementById("canvas1");
-    ctx1 = can1.getContext('2d');
-    can2 = document.getElementById("canvas2");
-    ctx2 = can2.getContext('2d');
-    can1.addEventListener('mousemove', onMouseMove, false);
-    bgPic.src = "./src/background.jpg";
-    canWidth = can1.width;
-    canHeight = can1.height;
-    ane = new aneObj();
-    ane.init();
-    fruit = new fruitObj();
-    fruit.init();
-    mom = new momObj();
-    mom.init();
-    baby = new babyObj();
-    baby.init();
-    mx = canWidth * 0.3;
-    my = canHeight * 0.3;
+  can1 = document.getElementById('canvas1');
+  ctx1 = can1.getContext('2d');
+  can2 = document.getElementById('canvas2');
+  ctx2 = can2.getContext('2d');
+  can1.addEventListener('mousemove', onMouseMove, false);
+  bgPic.src = './src/background.jpg';
+  canWidth = can1.width;
+  canHeight = can1.height;
+  ane = new aneObj();
+  ane.init();
+  fruit = new fruitObj();
+  fruit.init();
+  mom = new momObj();
+  mom.init();
+  baby = new babyObj();
+  baby.init();
+  mx = canWidth * 0.3;
+  my = canHeight * 0.3;
 
-    for (var i = 0; i < 8; i++) {
-        babyTail[i] = new Image();
-        babyTail[i].src = "./src/bigTail" + i + ".png";
+  for (var i = 0; i < 8; i++) {
+    babyTail[i] = new Image();
+    babyTail[i].src = './src/bigTail' + i + '.png';
 
-    }
+  }
 
-    for (var i = 0; i < 2; i++) {
-        babyEye[i] = new Image();
-        babyEye[i].src = "./src/babyEye" + i + ".png";
+  for (var i = 0; i < 2; i++) {
+    babyEye[i] = new Image();
+    babyEye[i].src = './src/babyEye' + i + '.png';
 
-    }
-    for (var i = 0; i < 20 ; i++) {
-        babyBody[i] = new Image();
-        babyBody[i].src = "./src/babyFade" + i + ".png";
+  }
 
-    }
-    //大鱼
-    for (var i = 0; i < 8; i++) {
-        bigTail[i] = new Image();
-        bigTail[i].src = "./src/bigTail" + i + ".png";
-    }
-    for (var i = 0; i < 2; i++) {
-        bigEye[i] = new Image();
-        bigEye[i].src = "./src/bigEye" + i + ".png";
+  for (var i = 0; i < 20; i++) {
+    babyBody[i] = new Image();
+    babyBody[i].src = './src/babyFade' + i + '.png';
 
-    }
+  }
 
-    for (var i = 0; i < 8; i++) {
-        bigBodyOra[i] = new Image();
-        bigBodyOra[i].src = "./src/bigSwim" + i + ".png";
-        bigBodyBlue[i] = new Image();
-        bigBodyBlue[i].src = "./src/bigSwimBlue" + i + ".png";
-    }
-    data = new dataObj();
-    ctx1.fillStyle = "white";
-    ctx1.font = "8px Verdana";
-    ctx1.textAlign = "center";
-    ctx1.shadowBlur = 10;
-    ctx1.shadowColor = "white";
+  //大鱼
+  for (var i = 0; i < 8; i++) {
+    bigTail[i] = new Image();
+    bigTail[i].src = './src/bigTail' + i + '.png';
+  }
 
-    wave = new waveObj();
-    wave.init();
-    halo = new haloObj();
-    halo.init();
+  for (var i = 0; i < 2; i++) {
+    bigEye[i] = new Image();
+    bigEye[i].src = './src/bigEye' + i + '.png';
+
+  }
+
+  for (var i = 0; i < 8; i++) {
+    bigBodyOra[i] = new Image();
+    bigBodyOra[i].src = './src/bigSwim' + i + '.png';
+    bigBodyBlue[i] = new Image();
+    bigBodyBlue[i].src = './src/bigSwimBlue' + i + '.png';
+  }
+
+  data = new dataObj();
+  ctx1.fillStyle = 'white';
+  ctx1.font = '8px Verdana';
+  ctx1.textAlign = 'center';
+  ctx1.shadowBlur = 10;
+  ctx1.shadowColor = 'white';
+
+  wave = new waveObj();
+  wave.init();
+  halo = new haloObj();
+  halo.init();
 }
 
 function gameloop() {
-    window.requestAnimFrame(gameloop);
-    var now = Date.now();
-    deltaTime = now - lastTime;
-    lastTime = now;
-    if (deltaTime > 40) {
-        deltaTime = 40;
-    } //解决切换页面时 果实不断变大的问题
-    drawbackground();
-    ane.draw();
-    fruitMonitor();
-    fruit.draw();
-    ctx1.clearRect(0, 0, canWidth, canHeight);
-    mom.draw();
-    momFruitCollision();
-    momBabyCollision();
-    baby.draw();
-    data.draw();
-    wave.draw();
-    halo.draw();
+  window.requestAnimFrame(gameloop);
+  var now = Date.now();
+  deltaTime = now - lastTime;
+  lastTime = now;
+  deltaTime = deltaTime > 40 ? 40 : deltaTime; //解决切换页面时 果实不断变大的问题
+  drawbackground();
+  ane.draw();
+  fruitMonitor();
+  fruit.draw();
+  ctx1.clearRect(0, 0, canWidth, canHeight);
+  mom.draw();
+  momFruitCollision();
+  momBabyCollision();
+  baby.draw();
+  data.draw();
+  wave.draw();
+  halo.draw();
 }
 
 function onMouseMove(e) {
-   
-    if (!data.gameOver) {
-        if (e.offsetX || e.layerX) {
-            mx = e.offsetX == undefined ? e.layerX : e.offsetX;
-            my = e.offsetY == undefined ? e.layerY : e.offsetY;
 
-
-        }
-    }
-
+  if (data.gameOver) return;
+  if (!e.offsetX && e.layerY) return;
+  mx = e.offsetX || e.layerX;
+  my = e.offsetY || e.layerY;
 }
--- js/mom.js
+++ js/mom.js
@@ -1,77 +1,76 @@
 var momObj = function() {
-    this.x;
-    this.y;
-    this.angle;
-    this.bigEye = new Image();
-    this.bigBody = new Image();
-    this.bigTail = new Image();
+  this.x;
+  this.y;
+  this.angle;
+  this.bigEye = new Image();
+  this.bigBody = new Image();
+  this.bigTail = new Image();
 
-    this.bigTailTimer = 0;
-    this.bigTailCount = 0;
+  this.bigTailTimer = 0;
+  this.bigTailCount = 0;
 
-    this.bigEyeTimer = 0;
-    this.bigEyeCount = 0;
-    this.bigEyeInterval = 1000;
-    this.bigBodyCount = 0;
-}
+  this.bigEyeTimer = 0;
+  this.bigEyeCount = 0;
+  this.bigEyeInterval = 1000;
+  this.bigBodyCount = 0;
+};
 
 momObj.prototype.init = function() {
-    this.x = canWidth * 0.5;
-    this.y = canHeight * 0.5;
-    this.angle = 0;
-    this.bigBody.src = "./src/bigSwim0.png"
+  this.x = canWidth * 0.5;
+  this.y = canHeight * 0.5;
+  this.angle = 0;
+  this.bigBody.src = './src/bigSwim0.png';
 
-}
+};
 
 momObj.prototype.draw = function() {
-    //lerp x,y
-    this.x = lerpDistance(mx, this.x, 0.99);
-    this.y = lerpDistance(my, this.y, 0.99);
-    //delta angle 角度差
-    //Math.atan2(y,x);
-    var deltaY = my - this.y;
-    var deltaX = mx - this.x;
-    var beta = Math.atan2(deltaY, deltaX) + Math.PI; //-PI,PI;
-    //lerp angle
-    this.angle = lerpAngle(beta, this.angle, 0.4);
-    //mom tail  count
-    this.bigTailTimer += deltaTime;
-    if (this.bigTailTimer > 50) {
+  //lerp x,y
+  this.x = lerpDistance(mx, this.x, 0.99);
+  this.y = lerpDistance(my, this.y, 0.99);
 
-        this.bigTailCount = (this.bigTailCount + 1) % 8;
-        this.bigTailTimer %= 50;
+  //delta angle 角度差
+  //Math.atan2(y,x);
+  var deltaY = my - this.y;
+  var deltaX = mx - this.x;
+  var beta = Math.atan2(deltaY, deltaX) + Math.PI; //-PI,PI;
+  //lerp angle
+  this.angle = lerpAngle(beta, this.angle, 0.4);
 
-    }
-    //mom eye count
-    this.bigEyeTimer += deltaTime;
-    if (this.bigEyeTimer > this.bigEyeInterval) {
+  //mom tail  count
+  this.bigTailTimer += deltaTime;
+  if (this.bigTailTimer > 50) {
 
-        this.bigEyeCount = (this.bigEyeCount + 1) % 2;
-        this.bigEyeTimer %= this.bigEyeInterval;
-        if (this.bigEyeCount == 0) {
-            this.bigEyeInterval = Math.random() * 1500 + 2000; //时间间隔为 [2000ms,3500ms)
-        } else {
-            this.bigEyeInterval = 200;
-        }
+    this.bigTailCount = (this.bigTailCount + 1) % 8;
+    this.bigTailTimer %= 50;
 
-    }
-    ctx1.save();
-    ctx1.translate(this.x, this.y);
-    ctx1.scale(0.5, 0.5);
-    ctx1.rotate(this.angle);
-    var bigTailCount = this.bigTailCount;
-    ctx1.drawImage(bigTail[bigTailCount], -bigTail[bigTailCount].width * 0.5 + 30, -bigTail[bigTailCount].height * 0.5);
-    var bigBodyCount = this.bigBodyCount;
-    if (data.double == 1) {
-        ctx1.drawImage(bigBodyOra[bigBodyCount], -bigBodyOra[bigBodyCount].width * 0.5, -bigBodyOra[bigBodyCount].height * 0.5);
+  }
 
-    } else {
-        ctx1.drawImage(bigBodyBlue[bigBodyCount], -bigBodyBlue[bigBodyCount].width * 0.5, -bigBodyBlue[bigBodyCount].height * 0.5);
+  //mom eye count
+  this.bigEyeTimer += deltaTime;
+  if (this.bigEyeTimer > this.bigEyeInterval) {
 
-    }
-    var bigEyeCount = this.bigEyeCount;
-    ctx1.drawImage(bigEye[bigEyeCount], -bigEye[bigEyeCount].width * 0.5, -bigEye[bigEyeCount].height * 0.5);
+    this.bigEyeCount = (this.bigEyeCount + 1) % 2;
+    this.bigEyeTimer %= this.bigEyeInterval;
+    this.bigEyeInterval = this.bigEyeCount == 0 ? Math.random() * 1500 + 2000 : 200;
+  }
 
+  ctx1.save();
+  ctx1.translate(this.x, this.y);
+  ctx1.scale(0.5, 0.5);
+  ctx1.rotate(this.angle);
+  var bigTailCount = this.bigTailCount;
+  ctx1.drawImage(bigTail[bigTailCount], -bigTail[bigTailCount].width * 0.5 + 30, -bigTail[bigTailCount].height * 0.5);
+  var bigBodyCount = this.bigBodyCount;
+  if (data.double == 1) {
+    ctx1.drawImage(bigBodyOra[bigBodyCount], -bigBodyOra[bigBodyCount].width * 0.5, -bigBodyOra[bigBodyCount].height * 0.5);
 
-    ctx1.restore();
-}
+  } else {
+    ctx1.drawImage(bigBodyBlue[bigBodyCount], -bigBodyBlue[bigBodyCount].width * 0.5, -bigBodyBlue[bigBodyCount].height * 0.5);
+
+  }
+
+  var bigEyeCount = this.bigEyeCount;
+  ctx1.drawImage(bigEye[bigEyeCount], -bigEye[bigEyeCount].width * 0.5, -bigEye[bigEyeCount].height * 0.5);
+
+  ctx1.restore();
+};
